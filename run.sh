c88d5bf6ec3460947857a48ef09ef4d3ec0fd917
Ibnu Yahya <anak10thn@gmail.com>
2018-04-23 07:33:25 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-kubectl create -f 01-service.yml
-kubectl create -f 02-service.yml
+kubectl --kubeconfig=/home/k8s/.kube/config create -f 01-service.yml
+kubectl --kubeconfig=/home/k8s/.kube/config create -f 02-service.yml
+cat << EOF >> /bin/kube-token
+#!/bin/bash
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+EOF
+chmod +x /bin/kube-token
